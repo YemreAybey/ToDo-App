4ca745a14628cd73a17f9225539504ba7544117a
Afani Martin <afanimartin@localhost.localdomain>
2019-09-30 13:24:01 +0300

    Add todo list logic

---
+++ src/modules/todo.js
@@ -0,0 +1,11 @@
+class ToDo {
+  constructor(title, date, status, descr = 'Dont Forget', isDone) {
+    this.title = title;
+    this.date = date;
+    this.status = status;
+    this.descr = descr;
+    this.isDone = false;
+  }
+}
+​
+export default ToDo;
